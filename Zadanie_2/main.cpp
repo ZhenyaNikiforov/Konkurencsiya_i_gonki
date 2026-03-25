b11ec579f8246a52106de65db0eed479844f7ebd
Zhenya Nikiforov <12A92@mail.ru>
2026-03-25 19:07:23 +0500

    Второе задание

---
+++ Zadanie_2/main.cpp
@@ -0,0 +1,134 @@
+/*- Подпрограмма, посвящённая прогресс-барам. -*/
+
+/*-
+номер потока по порядку;
+
+• идентификатор потока;
+
+• заполняющийся индикатор наподобие прогресс-бара, визуализирующий процесс «расчёта»;
+
+• после завершения работы каждого потока в соответствующей строке суммарное время, затраченное на работу потока.
+
+-*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <cstdlib>
+#include <iomanip>
+#include <windows.h>
+using namespace std;
+
+int threadsQuantity = 10;
+int threadLength = 50;
+
+vector<string> threadList(threadsQuantity, string(threadLength, '-'));
+vector<int> threadsID(threadsQuantity, 0);
+vector<double> threadsTime(threadsQuantity, 0);
+
+mutex mtx;
+
+void fillBar(int threadNumber)
+{
+  threadsID[threadNumber] = static_cast<int>(GetCurrentThreadId());
+  auto startTime = chrono::high_resolution_clock::now();
+
+  for (int symbolCount = 0; symbolCount < threadLength; ++symbolCount)
+  {
+    this_thread::sleep_for(chrono::seconds(1));
+    lock_guard<mutex> lock(mtx);
+    threadList[threadNumber][symbolCount] = '*';
+
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+
+    cout << setw(5) << "NUM"
+         << setw(10) << "ID"
+         << setw(threadLength + 2) << "PROGRESS_BAR"
+         << setw(15) << "TIME"
+         << endl;
+
+    for (int threadsCount = 0; threadsCount < threadsQuantity; ++threadsCount)
+    {
+      cout << setw(5) << threadsCount + 1
+           << setw(10) << threadsID[threadsCount]
+           << setw(threadLength + 2) << threadList[threadsCount]
+           << setw(15) << " "
+           << endl;
+    }
+  }
+
+  auto endTime = chrono::high_resolution_clock::now();
+  chrono::duration<double> resultTime = endTime - startTime;
+  threadsTime[threadNumber] = resultTime.count();
+
+  return;
+}
+
+int main()
+{
+  vector<thread> threads;
+
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+
+  cout << setw(5) << "NUM"
+       << setw(10) << "ID"
+       << setw(threadLength + 2) << "PROGRESS_BAR"
+       << setw(15) << "TIME"
+       << endl;
+
+  for (int threadsCount = 0; threadsCount < threadsQuantity; ++threadsCount)
+  {
+    cout << setw(5) << threadsCount + 1
+         << setw(10) << " "
+         << setw(threadLength + 2) << threadList[threadsCount]
+         << setw(15) << " "
+         << endl;
+  }
+
+  for (int threadsCount = 0; threadsCount < threadsQuantity; ++threadsCount)
+  {
+    threads.emplace_back(fillBar, threadsCount);
+  };
+
+  for (auto &t : threads)
+  {
+    if (t.joinable())
+    {
+      t.join();
+    };
+  };
+
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+
+  cout << setw(5) << "NUM"
+       << setw(10) << "ID"
+       << setw(threadLength + 2) << "PROGRESS_BAR"
+       << setw(15) << "TIME"
+       << endl;
+
+  for (int threadsCount = 0; threadsCount < threadsQuantity; ++threadsCount)
+  {
+    cout << setw(5) << threadsCount + 1
+         << setw(10) << threadsID[threadsCount]
+         << setw(threadLength + 2) << threadList[threadsCount]
+         << setw(15) << threadsTime[threadsCount]
+         << endl;
+  }
+
+  return 0;
+}
